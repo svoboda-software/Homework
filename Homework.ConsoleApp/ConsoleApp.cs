b78a239b4513df3167356448aaaaa7f231fe9af3
Anthony Svoboda <svobodasoftware@gmail.com>
2021-09-26 16:47:35 -0700

    Created namespaces and created console app.

---
+++ Homework.ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Homework.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("This is step 1 of my homework assignment.");
+			}
+			catch (System.Exception ex)
+			{
+				throw ex;
+			}
+		}
+	}
+}

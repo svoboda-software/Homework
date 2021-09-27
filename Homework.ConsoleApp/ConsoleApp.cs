3710c3a6647fcba6249886bf1c710bef0ffbd927
Anthony Svoboda <svobodasoftware@gmail.com>
2021-09-26 17:18:36 -0700

    Created pseudocode solution.
    This is a simple, procedural approach to satisfying the Acceptance Criteria in the least elegant way.
    
    This outline helps identify any services that need to be created, and the actions those services will perform. We can even begin to define the inputs and outputs for those actions.
    
    This is the current output of the console app for step 1 of the homework assignment:
    
       ------------
       App Started
       ------------
    
          Sort 1 - Started.
          Sort 1 - Complete.
    
          Sort 2 - Started.
          Sort 2 - Complete.
    
          Sort 3 - Started.
          Sort 3 - Complete.
    
       ----------------
       App Completed
       ----------------

--- Homework.ConsoleApp/ConsoleApp.cs
+++ Homework.ConsoleApp/ConsoleApp.cs
@@ -8,7 +8,43 @@ namespace Homework.ConsoleApp
 		{
 			try
 			{
-				Console.WriteLine("This is step 1 of my homework assignment.");
+				Console.WriteLine();
+				Console.WriteLine("-----------");
+				Console.WriteLine("App Started");
+				Console.WriteLine("-----------");
+				Console.WriteLine();
+
+				// var records = GetRecords();
+
+				Console.WriteLine("   Sort 1 - Started.");
+
+				// var sortFields = "by FavoriteColor, then by LastName";
+				// var sortedRecords = SortRecords(records, sortFields);
+				// DisplayRecords(sortedRecords);
+
+				Console.WriteLine("   Sort 1 - Complete.");
+				Console.WriteLine();
+				Console.WriteLine("   Sort 2 - Started.");
+
+				// var sortFields = "by DateOfBirth";
+				// var sortedRecords = SortRecords(records, sortFields);
+				// DisplayRecords(sortedRecords);
+
+				Console.WriteLine("   Sort 2 - Complete.");
+				Console.WriteLine();
+				Console.WriteLine("   Sort 3 - Started.");
+
+				// var sortFields = "by LastName desc";
+				// var sortedRecords = SortRecords(records, sortFields);
+				// DisplayRecords(sortedRecords);
+
+				Console.WriteLine("   Sort 3 - Complete.");
+
+				Console.WriteLine();
+				Console.WriteLine("-------------");
+				Console.WriteLine("App Completed");
+				Console.WriteLine("-------------");
+				Console.WriteLine();
 			}
 			catch (System.Exception ex)
 			{

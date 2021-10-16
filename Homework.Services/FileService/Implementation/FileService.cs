1d433b100017cbbbfcec3ea160e45d0677e3699d
Anthony Svoboda <svobodasoftware@gmail.com>
2021-10-16 12:50:10 -0700

    Consume the delimiter service from the file service.

--- Homework.Services/FileService/Implementation/FileService.cs
+++ Homework.Services/FileService/Implementation/FileService.cs
@@ -1,4 +1,6 @@
 using Homework.Data.Repositories.FileRepository;
+using Homework.Services.DelimiterService;
+using Homework.Services.DelimiterService.Models;
 using Homework.Services.FileService.Models;
 using Homework.Shared.Models;
 using System.Collections.Generic;
@@ -8,17 +10,19 @@ namespace Homework.Services.FileService.Implementation
 	public class FileService : IFileService
 	{
 		private IFileRepository repo { get; }
+		private IDelimiterService delimiterService { get; set; }
 
-		public FileService(IFileRepository repo)
+		public FileService(IFileRepository repo, IDelimiterService delimiterService)
 		{
 			this.repo = repo;
+			this.delimiterService = delimiterService;
 		}
 
 		public GetFilesResponse GetFiles(GetFilesRequest request)
 		{
 			var files = new List<File>();
 
-			// TODO: Use a delimiter service to return the list of delimiters used by business.
+			var delimeters = delimiterService.GetDelimiters(new GetDelimitersRequest())?.Delimiters;
 			// TODO: Call the file repo to get the path from each delimiter.
 
 			return new GetFilesResponse

f3462ede577a1f09634be90522cf0247603dd85d
Anthony Svoboda <svobodasoftware@gmail.com>
2021-10-19 16:53:31 -0700

    Remove File and Delimiter logic from Record repository.

--- Homework.Data/Repositories/RecordRepository/Implementation/RecordRepository.cs
+++ Homework.Data/Repositories/RecordRepository/Implementation/RecordRepository.cs
@@ -1,8 +1,6 @@
 using Homework.Data.Repositories.RecordRepository.Models;
 using Homework.Shared.Extensions;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace Homework.Data.Repositories.RecordRepository.Implementation
@@ -11,24 +9,16 @@ namespace Homework.Data.Repositories.RecordRepository.Implementation
 	{
 		public RecordRepository() { }
 
-		#region "Public methods"
+		#region Public methods
 
 		/// <summary>
-		/// Returns all records from the given files.
+		/// Returns all records from the given value array.
 		/// <summary>
 		public GetRecordsResponse GetRecords(GetRecordsRequest request)
 		{
-			var records = new List<Record>();
-
-			request?.Files?
-				.ForEach(f =>
-					records.AddRange(
-					// Lazy load the file with File.ReadLines() instead of loading the entire file into memory with File.ReadAllLines().
-					File.ReadLines(f.Path)
-					.Skip(1)
-					.Select(s => s.Split(f.Delimiter))
-					.Select(s => ToRecord(s))
-					.ToList()));
+			var records = request.ValueArrays
+				.Select(s => ToRecord(s))
+				.ToList();
 
 			return new GetRecordsResponse
 			{
@@ -54,20 +44,20 @@ namespace Homework.Data.Repositories.RecordRepository.Implementation
 		}
 		#endregion
 
-		#region "Private methods"
+		#region Private methods
 
 		private Record ToRecord(string[] values)
 		{
 			// Trim all values.
-			values = values.Select(s => s.Trim()).ToArray();
+			var v = values?.Select(s => s.Trim()).ToArray();
 
 			return new Record
 			{
-				LastName = values[0],
-				FirstName = values[1],
-				Email = values[2],
-				FavoriteColor = values[3],
-				DateOfBirth = Convert.ToDateTime(values[4])
+				LastName = v?[0],
+				FirstName = v?[1],
+				Email = v?[2],
+				FavoriteColor = v?[3],
+				DateOfBirth = Convert.ToDateTime(v[4])
 			};
 		}
 		#endregion

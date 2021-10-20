56a8d3f9d30e0915ba59fdcd319c6f3e8f2eb215
Anthony Svoboda <svobodasoftware@gmail.com>
2021-10-20 14:37:55 -0700

    Create RecordController.cs and QueryRecords endpoint
    A property name following the Record controller's 'home' route will query records by the given property.

---
+++ Homework.Api/Controllers/RecordsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Homework.Api.Models;
+using Homework.Services.RecordService;
+using Homework.Services.RecordService.Models;
+
+namespace Homework.Api.Controllers
+{
+	[ApiController]
+	public class RecordsController : Controller
+	{
+		private readonly IRecordService recordService;
+
+		public RecordsController(IRecordService recordService)
+		{
+			this.recordService = recordService;
+		}
+
+		[Route("[controller]/{propertyName?}")]
+		[HttpGet]
+		public ApiResponse<QueryPropertyResponse> QueryRecords(string propertyName)
+		{
+			var response = new ApiResponse<QueryPropertyResponse>();
+			response.Data = recordService.QueryProperty(new QueryPropertyRequest { PropertyName = propertyName });
+			response.StatusCode = StatusCodes.Status200OK;
+
+			return response;
+		}
+	}
+}
